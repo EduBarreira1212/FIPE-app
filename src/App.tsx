01253ecddabbdcc47e88f637f6a5697fa841b8d5
Eduardo Barreira <edubarreira1212@gmail.com>
2025-04-06 21:35:17 -0300

    refactor: move App component to src directory and update imports

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>FIPE App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
