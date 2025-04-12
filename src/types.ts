32d2535081f0e1a249830c94a7b170c223f47bef
Eduardo Barreira <edubarreira1212@gmail.com>
2025-04-12 20:35:37 -0300

    feat: create file to define types for Brand, Year, and Model

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export type Brand = {
+  code: string;
+  name: string;
+};
+
+export type Year = {
+  code: string;
+  name: string;
+};
+
+export type Model = {
+  code: string;
+  name: string;
+};
