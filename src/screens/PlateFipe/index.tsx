bac016bd4df5b8ca4dbf03413db723a70fd1f039
Eduardo Barreira <edubarreira1212@gmail.com>
2025-04-14 22:55:55 -0300

    feat: create PlateFipe screen

---
+++ src/screens/PlateFipe/index.tsx
@@ -0,0 +1,8 @@
+import { Text } from '@react-navigation/elements';
+import React from 'react';
+
+const PlateFipeScreen = () => {
+  return <Text>PlateFipeScreen</Text>;
+};
+
+export default PlateFipeScreen;
