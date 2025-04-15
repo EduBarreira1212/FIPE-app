8b5cf5ca477fe437b26dfd0084d0ebb517116408
Eduardo Barreira <edubarreira1212@gmail.com>
2025-04-14 22:52:12 -0300

    feat: enhance HomeScreen with navigation to VehicleFipe

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,15 @@
-import { Text } from '@react-navigation/elements';
+import { Button, Text } from '@react-navigation/elements';
 import { View } from 'react-native';
+import { RootStackParamList } from '../../types';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-const HomeScreen = () => {
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+const HomeScreen = ({ navigation }: HomeScreenProps) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button onPress={() => navigation.navigate('VehicleFipe')}>Search FIPE by vehicle</Button>
     </View>
   );
 };
