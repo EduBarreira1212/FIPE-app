e0d00af3bd085827de524b993429e1c83fb0ea12
Eduardo Barreira <edubarreira1212@gmail.com>
2025-04-16 22:45:18 -0300

    feat: enhance Home screen layout and replace buttons with styled touchable components

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,27 @@
-import { Button, Text } from '@react-navigation/elements';
-import { View } from 'react-native';
-import { RootStackParamList } from '../../types';
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { Text } from '@react-navigation/elements';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../../types';
+import { styles } from './styles';
 
 type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 const HomeScreen = ({ navigation }: HomeScreenProps) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button onPress={() => navigation.navigate('VehicleFipe')}>Search FIPE by vehicle</Button>
-      <Button onPress={() => navigation.navigate('PlateFipe')}>Search FIPE by plate</Button>
+    <View style={styles.container}>
+      <View style={styles.card}>
+        <Text style={styles.title}>Bem-vindo</Text>
+        <Text style={styles.subtitle}>Como você deseja saber o valor da FIPE?</Text>
+
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('VehicleFipe')}>
+          <Text style={styles.buttonText}>🔍 Procurar FIPE pelo veículo</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('PlateFipe')}>
+          <Text style={styles.buttonText}>🚗 Procurar FIPE pela placa do veículo</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
