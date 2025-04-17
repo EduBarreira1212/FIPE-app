ed1b0a4e2232f978c5db97f26a6fad99ae3ee30b
Eduardo Barreira <edubarreira1212@gmail.com>
2025-04-16 22:45:28 -0300

    feat: refactor styles for VehicleFipe screen with improved layout and design

--- src/screens/VehicleFipe/styles.ts
+++ src/screens/VehicleFipe/styles.ts
@@ -1,22 +1,48 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({
-    container: {
-      marginTop: 50,
-      padding: 20,
-    },
-    input: {
-      borderWidth: 1,
-      borderColor: '#ccc',
-      borderRadius: 5,
-      padding: 10,
-      marginBottom: 10,
-    },
-    option: {
-      padding: 10,
-      borderBottomWidth: 1,
-      borderColor: '#eee',
-    },
-  });
-
-export default styles;
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#f9f9f9',
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: '600',
+    marginTop: 12,
+    marginBottom: 4,
+    color: '#333',
+  },
+  input: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 10,
+    fontSize: 16,
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+  option: {
+    padding: 12,
+    backgroundColor: '#fff',
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+  },
+  clearButton: {
+    marginTop: 20,
+    backgroundColor: '#e74c3c',
+    borderRadius: 8,
+  },
+  resultContainer: {
+    marginTop: 20,
+    backgroundColor: '#fff',
+    padding: 16,
+    borderRadius: 8,
+    elevation: 2,
+  },
+  resultText: {
+    fontSize: 16,
+    marginBottom: 8,
+    color: '#333',
+  },
+});
